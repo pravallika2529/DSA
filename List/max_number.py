8267960e018432ac632cbd500fa49169b8ffc2d8
pravallika <pravallikau2529@gmail.com>
2026-07-08 18:44:26 +0530

    Added max_number.py program

--- List/max_number.py
+++ List/max_number.py
@@ -1,6 +1,16 @@
-a = 10
-b = 25
-if a > b:
-  print("Maximum: ", a)
-else:
-  print("Maximum: ", b)
+# finding the maximum number in a list
+
+# brute force way
+lst = [3, 5, 2, 8, 1, 9, 4]
+max_num = lst[0]
+for item in lst:
+    if item > max_num:
+        max_num = item
+
+print("The maximum number is: ", max_num)
+# time complexity --> O(n) --> this is because the loop visits each element in the list exactly once, so O(n)
+# space complexity --> O(1) --> the alg doesnt create another list or use extra memory that grows with the input size
+# only one extra variable is used
+# hence, the space complexity is O(1)
+
+# this is the optimized way only, cuz all elements need to be checked
