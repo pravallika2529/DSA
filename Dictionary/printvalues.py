3626418543990176acc16a617cd01d713676dca9
pravallika <pravallikau2529@gmail.com>
2026-07-23 22:12:52 +0530

    Added program to print values in dictionary

---
+++ Dictionary/printvalues.py
@@ -0,0 +1,9 @@
+# to print all values in dictionary
+
+dct = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+
+for value in dct.values():
+    print(value, end = " ")
+
+# time complexity --> O(n)
+# space complexity --> O(1)
